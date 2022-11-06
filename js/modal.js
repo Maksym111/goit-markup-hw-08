890111be01b54a6e69a4a041cd70e9857ab3f393
Maksym Kikot <maxym1111111@gmail.com>
2022-11-06 03:46:52 +0200

    Зроблений css для смартфону

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,33 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('backdrop--is-hidden');
   }
+
+  function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+    refs.menu.classList.toggle('backdrop-menu--is-hidden');
+  }
+
+  // const menuBtnRef = document.querySelector('[data-menu]');
+
+  // menuBtnRef.addEventListener('click', () => {
+  //   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+  //   menuBtnRef.classList.toggle('backdrop-menu--is-hidden');
+  //   menuBtnRef.setAttribute('aria-expanded', !expanded);
+  // });
 })();
